be997c664b6c1a16a38600d01895e5ee9130afeb
SerjVolynets <>
2020-12-28 22:14:49 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import PhoneIphoneIcon from "@material-ui/icons/PhoneIphone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <div className="headerInfoContainer">
+          <div className="headerContainer">
+            <div className="headerElementStyle">
+              <PhoneIphoneIcon /> <p>+8 800 555 34 55</p>
+            </div>
+            <div className="headerElementStyle">
+              <MailOutlineIcon /> <p>erek.group@gmail.com</p>
+            </div>
+            <div>EREK GROUP</div>
+          </div>
+          <div>
+            <ShoppingBasketIcon />
+          </div>
+        </div>
       </header>
+      <div>content</div>
     </div>
   );
 }
