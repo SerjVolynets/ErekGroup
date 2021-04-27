3af8958e3b79c918a3dfe52ec5ffccbfe86ac075
SerjVolynets <sergius.volynets@gmail.com>
2021-04-27 12:07:20 +0300

    Last changes

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ function App() {
     <div className="App">
       <Header />
       <MenuCatalog />
+{/*                                                               */}
       <div className="imageBlock">
         <img src={toolsImg} alt="Tools" width="100%" height="100%" />
         <div className="plate">
-          <h1 className="textPlate">Воплощайте идеи</h1>
-          <h2 className="textPlate">Турецкое и русское катчество</h2>
-          <p className="textPlate"> Надежные инструменты для профессиональных работ</p>
+          <h1 className="textPlate">Воплощайте идеи!</h1>
+          <p className="textPlate">Катчественные материалы.</p>
+          <p className="textPlate"> Надежные инструменты для профессиональных работ.</p>
         </div>
       </div>
+
+      <div className='textInfo'>
+        <div className='textInfoBlock'>
+          <h2 className='textFont'>Добро пожаловать в наш интернет-магазин стройматериалов</h2>
+          <p className='textFont'>Мы с радостью обслуживаем заказы через сайт 5 дней в неделю с 9 до 18 по московскому времени.</p>
+          <p className='textFont'>Доставка возможна по Москве. Вы можете оставить заказ на сайте и наш менеджер свяжется с вами для уточнения вашего заказа</p>
+        </div>
+      </div>
+
+
     </div>
   );
 }
